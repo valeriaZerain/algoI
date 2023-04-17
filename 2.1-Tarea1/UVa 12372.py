c094a1702844c3669e75143eabcc3a97b2199318
valeriaZerain <126807138+valeriaZerain@users.noreply.github.com>
2023-04-17 17:15:41 -0400

    Update UVa 12372.py

--- 2.1-Tarea1/UVa 12372.py
+++ 2.1-Tarea1/UVa 12372.py
@@ -4,5 +4,5 @@ cases = int(next(inputs))
 for case in range(cases):
     lenght, width, height = map(int, next(inputs).split())
     if lenght > 20 or width > 20 or height > 20:
-        print('Case {}: bad\n'.format(case+1))
-    else: print('Case {}: good\n'.format(case+1))
+        print('Case {}: bad'.format(case+1))
+    else: print('Case {}: good'.format(case+1))
